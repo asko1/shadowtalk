8c91ed85c1646bad3d13983e580560a173c8982c
Artur Allmann <Artur.Allmann@tptlive.ee>
2023-02-02 20:18:57 +0200

    created voicechat item
    need to add link and add it to the page

---
+++ components/Voicechat.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import VoiceChatIcon from '@mui/icons-material/VoiceChat';
+
+export default function Voicechat() {
+    return(
+        <div>
+            <Button
+            startIcon={<VoiceChatIcon/>}>
+            </Button>
+        </div>
+    )
+}
