db07700d22176b7a8c53df1412b7fb10e406dcb3
Artur Allmann <Artur.Allmann@tptlive.ee>
2023-02-10 11:28:22 +0200

    created color palette for simplifying adding colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { theme } from '../utils/theme';
+import { ThemeProvider } from "@mui/material";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return( 
+  <ThemeProvider theme={theme} >
+    <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ utils/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+        palette: {
+            primary: {
+                light: '#42a5f5',
+                main: '#1976d2',
+            },
+            secondary: {
+                light: '#42f57c',
+                main: '#2e7d32',
+            }
+        }
+    })
