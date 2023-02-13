52e6a468b6bf72f38a19cfb7ec6c9411d81ddebe
Artur Allmann <Artur.Allmann@tptlive.ee>
2023-02-13 09:21:41 +0200

    reworked theme file

--- utils/theme.tsx
+++ utils/theme.tsx
@@ -1,18 +1,16 @@
-import { Typography } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
+import { ThemeOptions } from "@mui/material/styles";
 
-export const theme = createTheme({
-        palette: {
-            primary: {
-                light: '#42a5f5',
-                main: '#1976d2',
-            },
-            secondary: {
-                light: '#42f57c',
-                main: '#2e7d32',
-            }
+export const themeOptions: ThemeOptions = {
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#1976d2',
+        }, 
+        secondary: {
+            main: '#2e7d32',
         },
-        typography: {
-            "fontFamily": "'Nunito Sans', sans-serif"
-        },
-    })
+    },
+    typography: {
+        "fontFamily": "'Nunito Sans', sans-serif"
+    },
+};
