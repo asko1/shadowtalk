cd2ad7d5556fefa869a1e7b02a4a818dad65b34e
Artur Allmann <Artur.Allmann@tptlive.ee>
2023-05-22 11:02:23 +0300

    change state name and added made it so visibility doesn't get overwritten

--- components/Participants.tsx
+++ components/Participants.tsx
@@ -7,13 +7,13 @@ export default function Participants(props: any) {
   const ably = assertConfiguration();
   let [presenceData] = usePresence(props.channelName);
   const [andMore, setAndMore] = useState(0);
-  const [visible, setVisible] = useState("hidden");
+  const [isVisible, setIsVisible] = useState("hidden");
   const presenceList = presenceData.map((member: any, index: any) => {
     const isItMe = member.clientId === ably.auth.clientId ? " (me)" : "";
 
     if (index >= 7) {
+      if (isVisible != "visible") setIsVisible("visible");
       setAndMore(andMore + 1);
-      setVisible("visibile");
     } else {
       return (
         <li key={index} className={styles.participant}>
@@ -28,7 +28,7 @@ export default function Participants(props: any) {
   return (
     <div>
       <ul>{presenceList}</ul>
-      <Typography variant="h5" sx={{ visibility: visible }}>
+      <Typography variant="h5" sx={{ visibility: isVisible }}>
         And {andMore} more
       </Typography>
     </div>
