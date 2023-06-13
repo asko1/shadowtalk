2b1e47fc659abe08be3f3a1c89cd12726f4d4259
Sten P <parv.sten@gmail.com>
2023-06-13 16:59:58 +0300

    exit refresh

--- components/buttons/ExitButton.tsx
+++ components/buttons/ExitButton.tsx
@@ -3,11 +3,13 @@ import { IconButton } from "@mui/material";
 import homeStyle from "../../styles/Home.module.css";
 
 export default function ExitButton(props: any) {
+  const refresh = () => window.location.reload()
   return (
     <IconButton
       color="error"
       className={homeStyle.ExitButton}
       disabled={!props.kms}
+      onClick = { refresh }
     >
       <CancelIcon />
     </IconButton>
